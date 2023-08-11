af39740d05924c50baa30f968726dff97f67ced3
Kanagaraj-R <kanagarajram88@gmail.com>
2023-08-11 21:11:57 +0530

    commit
    Signed-off-by: Kanagaraj-R <kanagarajram88@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Persondeatils from "./components/Persondeatils";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>React Crud Operations</h1>
+      <h4>Create Update Delete Edit</h4>
+      <Persondeatils />
     </div>
   );
 }
